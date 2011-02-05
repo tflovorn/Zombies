5952971abe5dca9691cda3ddf025cdd98ba4a8f3
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-02-05 16:51:23 -0600

    initialization code pulled out of index.html

--- js/game.js
+++ js/game.js
@@ -1,4 +1,44 @@
-function Init_Game(canvasElement) {
+window.addEventListener("load", Init_Game, false);
+
+var WIDTH = 800, HEIGHT = 480;
+    canvas, context,
+    keyEnum, keyArray,
+    next_bullet, bullet_array, bullet_image,
+    zombie_array, player;
+
+function Init_Game() {
+    canvas = document.getElementById("game_canvas");
+    context = canvas.getContext("2d");
+            
+    keyEnum = { W_Key:0, A_Key:1, S_Key:2, D_Key:3, Space_Key:4};
+    keyArray=new Array(5);
+    keyArray[0]=false;
+    keyArray[1]=false;
+    keyArray[2]=false;
+    keyArray[3]=false;
+    keyArray[4]=false;
+            
+    next_bullet=0;
+            
+    bullet_array=new Array(100);
+    bullet_image = new Image();
+    bullet_image.src = "images/bullet.png";
+
+    zombie_array=new Array(100);
+    var zombie;
+    for (var i=0;i<10;++i)
+    {
+        for (var j=0;j<10;++j)
+        {
+            zombie=new Character(i*100,j*100,0,0,1/(i+j)+.1,"images/zombie.png");
+            zombie_array[i*10+j]=zombie;
+        }
+    }
+    player=new Character(100,300,0,0,3,"images/player.png");
+            
+    window.addEventListener("keydown",doKeyDown,true);
+    window.addEventListener("keyup",doKeyUp,true);
+
     setInterval("Draw()", 10);
     setInterval("Update()", 10);
 }
